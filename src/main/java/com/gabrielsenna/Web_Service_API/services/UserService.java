1ac76b804e34491e07e4e8fb55074605ca5d3874
Gabriel <gabrielmagalhaes3344@gmail.com>
2024-06-10 20:06:38 -0300

    Implemented: Service layer and component registration

---
+++ src/main/java/com/gabrielsenna/Web_Service_API/services/UserService.java
@@ -0,0 +1,25 @@
+package com.gabrielsenna.Web_Service_API.services;
+
+import com.gabrielsenna.Web_Service_API.entities.User;
+import com.gabrielsenna.Web_Service_API.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
