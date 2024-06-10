4de9a4b554bb0a89544159550e9e6f7736ea385d
Gabriel Senna <gabrielmagalhaes3344@gmail.com>
2024-06-10 16:13:41 -0300

    Project created!

---
+++ src/main/java/com/gabrielsenna/Web_Service_API/WebServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielsenna.Web_Service_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceApiApplication.class, args);
+	}
+
+}
